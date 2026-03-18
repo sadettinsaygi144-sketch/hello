44e9c4b7cc68cfc92c7bf43f9a9c28f32681a9d5
sadettinsaygi144-sketch <sadettinsaygi144@gmail.com>
2026-03-18 13:36:04 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,18 +6,16 @@ app = Flask(_name_)
 
 DATABASE_URL = os.getenv("DATABASE_URL", "")
 HTML ="""
-
 <! doctype html>
-
 <html>
 <head>
-<title>Buluttan Selam !< /title>
-<style>
-body { font-family: Arial; text-align: center; padding: 50px; background: #eef2f3; }
-h1 { color: #333; }
-form { margin: 20px auto; }
-input { padding: 10px; font-size: 16px; }
-button { padding: 10px 15px; background: #4CAF50; color: white; border: none; border-radius: 6px; cursor: pointer; }
-ul { list-style: none; padding: 0; }
-li { background: white; margin: 5px auto; width: 200px; padding: 8px; border-radius: 5px; }
+ <title>Buluttan Selam !< /title>
+ <style>
+  body { font-family: Arial; text-align: center; padding: 50px; background: #eef2f3; }
+  h1 { color: #333; }
+  form { margin: 20px auto; }
+  input { padding: 10px; font-size: 16px; }
+  button { padding: 10px 15px; background: #4CAF50; color: white; border: none; border-radius: 6px; cursor: pointer; }
+  ul { list-style: none; padding: 0; }
+  li { background: white; margin: 5px auto; width: 200px; padding: 8px; border-radius: 5px; }
 </style>
